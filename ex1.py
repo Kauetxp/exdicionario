90815e94285316538ebf3123c89f7b0c4d6e2ecd
Kauetxp <123607569+Kauetxp@users.noreply.github.com>
2023-04-13 01:16:03 +0000

    To quase

--- ex1.py
+++ ex1.py
@@ -1,2 +1,19 @@
 #Escreva uma função que receba itens (números) em um dicionário como entrada e retorne a chave com o maior valor.
 
+dicionario = {}
+
+def recebe():
+    
+    while True:
+
+        chave = input("Digite uma palavra: ")
+        item = int(input("Atribua um número a ele: "))
+        dicionario[chave]=item
+        c = input("Deseja adicionar mais itens? (sim/nao)")
+        if c == 'nao':
+            break
+    n = sorted(dicionario.values())
+    n = n[-1]
+    print(n)
+
+recebe()
